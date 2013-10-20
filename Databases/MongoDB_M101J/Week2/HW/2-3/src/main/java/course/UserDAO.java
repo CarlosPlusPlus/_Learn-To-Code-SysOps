7da7bf7c14080c3e0f038948eb74f3592bc527f9
cjlwired <carlos.jose.lazo@gmail.com>
2013-10-20 18:08:40 -0400

    Complete M101J HW2-3.

--- Databases/MongoDB_M101J/Week2/HW/2-3/src/main/java/course/UserDAO.java
+++ Databases/MongoDB_M101J/Week2/HW/2-3/src/main/java/course/UserDAO.java
@@ -48,16 +48,23 @@ public class UserDAO {
         // be sure to add username and hashed password to the document. problem instructions
         // will tell you the schema that the documents must follow.
 
-
+        DBObject doc = new BasicDBObject("_id", username)
+            .append("password", passwordHash);
 
         if (email != null && !email.equals("")) {
             // XXX WORK HERE
             // if there is an email address specified, add it to the document too.
+
+            doc = new BasicDBObject("_id", username)
+                .append("password", passwordHash)
+                .append("email",email);
         }
 
         try {
             // XXX WORK HERE
             // insert the document into the user collection here
+
+            usersCollection.insert(doc);
             return true;
         } catch (MongoException.DuplicateKey e) {
             System.out.println("Username already in use: " + username);
@@ -70,6 +77,8 @@ public class UserDAO {
 
         // XXX look in the user collection for a user that has this username
         // assign the result to the user variable.
+
+        user = usersCollection.findOne(new BasicDBObject("_id",username));
 
         if (user == null) {
             System.out.println("User not in database");
